0369829b3d499e13fcbbb3690ea7e919163ff2f0
Manoj Mishra <b0rn2c0d3@gmail.com>
2013-01-22 19:42:57 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VimSnippets::Application.config.secret_token = '43525f9bf4909c8b770317a00c6f2063baf404d641b944d4d69a8e1177e5953f9dcc098e6b00456c5273f07ea40de66e93de129c3f25313e000294345c6a1c44'
